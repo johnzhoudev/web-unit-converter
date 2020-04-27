bb61a868d14f1ac502fd3b9477779c8beadf950d
John Zhou <john2001@live.ca>
2020-04-26 21:38:53 -0400

    Updates

---
+++ precache-manifest.a2fa965091d775ff45b462e6f223a9d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17956b70e656be050640f4e4c810f3df",
+    "url": "/web-unit-converter/index.html"
+  },
+  {
+    "revision": "abce77bace999439e51f",
+    "url": "/web-unit-converter/static/css/main.900d7867.chunk.css"
+  },
+  {
+    "revision": "44bda59d3db8236b4f22",
+    "url": "/web-unit-converter/static/js/2.f01e5b2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-unit-converter/static/js/2.f01e5b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abce77bace999439e51f",
+    "url": "/web-unit-converter/static/js/main.fc16de50.chunk.js"
+  },
+  {
+    "revision": "df20024ca60886871ba8",
+    "url": "/web-unit-converter/static/js/runtime-main.5e6af578.js"
+  }
+]);
